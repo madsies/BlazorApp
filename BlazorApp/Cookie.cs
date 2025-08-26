7fefc94889d62a26c6ead0dbe157dc55b1dd2176
madsies <madsiesss@gmail.com>
2025-08-26 13:45:15 +0100

    Functioning "Cookie-Clicker"-like

--- BlazorApp/Cookie.cs
+++ BlazorApp/Cookie.cs
@@ -1,18 +1,69 @@
-using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
-
 namespace BlazorApp;
+
+public class CookieHelper
+{
+    public int basePrice;
+    public string helperName;
+    public int cps;
+    public int amount;
+
+    public CookieHelper(int price, string name, int cookiesPerSec)
+    {
+        basePrice = price;
+        helperName = name;
+        cps = cookiesPerSec;
+        amount = 0;
+    }
+
+
+    public void addHelper(int count)
+    {
+        amount += count;
+    }
+
+}
 
 public class Cookie
 {
     private int cookieCount = 0;
     private int clickStrength = 1;
+    private int CPS = 0;
+    public List<CookieHelper> helpers = new List<CookieHelper>();
 
-
-    public void Main()
+    public Cookie()
     {
-        
+        initialiseHelpers();
+    }
+
+    public void upgradeHelper(CookieHelper helper)
+    {
+        if (cookieCount >= helper.basePrice)
+        {
+            cookieCount -= helper.basePrice;
+            helper.addHelper(1);
+        }
+        calculateCPS();
+    }
+
+    public int getCPS()
+    {
+        return CPS;
+    }
+
+    private void calculateCPS()
+    {
+        int count = 0;
+        foreach (CookieHelper helper in helpers)
+        {
+            count += helper.cps * helper.amount;
+        }
+        CPS = count;
+    }
+
+    private void initialiseHelpers()
+    {
+        helpers.Add(new CookieHelper(10, "Grandma", 1));
+        helpers.Add(new CookieHelper(100, "Farm", 3));
     }
 
     public int getCookies()
